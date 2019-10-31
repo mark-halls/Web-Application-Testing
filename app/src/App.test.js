56ce331a5827508eea3ab2ec42dd7ab7fd93751f
Mark Halls <mark.halls@gmail.com>
2019-10-31 16:45:27 -0700

    Add tests

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("renders without crashing", () => {
+  render(<App />);
+});
+
+test("it displays a button with the text 'hit'", () => {
+  const { getByText } = render(<App />);
+  getByText(/hit/i);
+});
+
+test("it displays a 1 when click on ball", () => {
+  const { getAllByText, getByTestId } = render(<App />);
+  fireEvent.click(getAllByText(/ball/i)[1]);
+  expect(getByTestId("display-ball")).toHaveTextContent("1");
+});
+
+test("balls resets to 0 if clicked 4 times", () => {
+  const { getAllByText, getByTestId } = render(<App />);
+  for (let i = 0; i < 4; i++) {
+    fireEvent.click(getAllByText(/ball/i)[1]);
+  }
+  expect(getByTestId("display-ball")).toHaveTextContent("0");
 });
--- app/src/components/Display/index.jsx
+++ app/src/components/Display/index.jsx
@@ -4,8 +4,12 @@ const Display = props => {
   const { ball, strike } = props.scoreboard;
   return (
     <div>
-      <p>Balls: {ball}</p>
-      <p>Strikes: {strike}</p>
+      <p>
+        Balls: <span data-testid="display-ball">{ball}</span>
+      </p>
+      <p>
+        Strikes: <span data-testid="display-strike">{strike}</span>
+      </p>
     </div>
   );
 };
